7a183f2ae8e95829e24409b071f4040bae323956
aza <nguyenhai011101@gmail.com>
2025-10-20 23:20:20 +0700

    feat: add libs & implement ui login

---
+++ Client/app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-blue">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- Client/nuxt.config.ts
+++ Client/nuxt.config.ts
@@ -1,5 +1,30 @@
+import tailwindcss from "@tailwindcss/vite";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  css: ["~/assets/styles/main.css"],
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    "@nuxt/image",
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+    plugins: [tailwindcss()],
+  },
+});
