c602103d8ad779bcf7b31a3b3665e0f48213532c
aza <nguyenhai011101@gmail.com>
2025-11-08 13:55:30 +0700

    feat(client) add instructor detail ui

--- Client/app/components/courses/detailForStudent.vue
+++ Client/app/components/courses/detailForStudent.vue
@@ -3,14 +3,6 @@
     <v-card>
       <v-card-text>
         <div class="text-3xl font-medium">Ứng dụng phần mềm</div>
-        <div class="flex gap-3 mt-2 text-sm">
-          <div>
-            Created by
-            <nuxt-link to="/" class="font-bold text-blue-500">
-              Nguyễn Đức Dư
-            </nuxt-link>
-          </div>
-        </div>
         <div class="text-xs mt-1">Last updated: 01/11/2025</div>
 
         <div class="mt-8">
@@ -69,6 +61,48 @@
         Start learning
       </v-btn>
     </div>
+
+    <v-card>
+      <v-card-title class="font-normal text-base"> Instructor </v-card-title>
+      <v-card-text>
+        <nuxt-link to="/" class="font-medium text-blue-500 text-base">
+          Nguyễn Đức Dư
+        </nuxt-link>
+
+        <div>Ứng dụng phần mềm Instructor</div>
+
+        <div class="flex items-start gap-3 mt-3">
+          <nuxt-img
+            src="/vietnam.jpg"
+            width="100"
+            class="rounded-circle"
+            alt=""
+          />
+
+          <div class="grid grid-cols-[32px_1fr] gap-y-1">
+            <v-icon icon="mdi-star-outline" />
+            <div>4.6 Rating</div>
+
+            <v-icon icon="mdi-license" />
+            <div>584,287 Reviews</div>
+
+            <v-icon icon="mdi-account-multiple-outline " />
+            <div>1,935,274 Students</div>
+
+            <v-icon icon="mdi-play-circle-outline" />
+            <div>30 Courses</div>
+          </div>
+        </div>
+
+        <div class="mt-5">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+          eius rerum eveniet velit? Ad iure id esse eius vitae praesentium
+          molestias quibusdam ipsum. Iste totam enim molestiae minus, harum
+          eveniet tempora possimus minima accusantium dolor error a doloribus,
+          vel facilis?
+        </div>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
